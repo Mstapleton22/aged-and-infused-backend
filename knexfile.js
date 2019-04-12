4ba932a8080fd827982ef4a071d299ac502c73af
Mstapleton22 <mike.stapleton2@gmail.com>
2019-04-11 20:13:54 -0600

    deploying

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost:5432/aged_and_infused'
+    connection: 'postgresql://localhost:5432/aged_and_infused' || 'postgres://ljdzngeoabvxab:7e25c8fdbd80854df38f13736308c2510323d2a54703cd15b1a2de67a6346005@ec2-184-73-210-189.compute-1.amazonaws.com:5432/d4hn4763ug8ij0'
   },
 
 
