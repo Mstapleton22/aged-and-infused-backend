7a60ffa904461992ff51880fe36a63742c3d5a13
Mstapleton22 <mike.stapleton2@gmail.com>
2019-04-15 10:22:15 -0600

    added cocktails

--- seeds/liquor.js
+++ seeds/liquor.js
@@ -5,11 +5,36 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('liquor').insert([
-        { id: 1, name: 'whiskey' },
-        { id: 2, name: 'gin' },
-        { id: 3, name: 'vodka' },
-        { id: 4, name: 'tequila' },
-        { id: 5, name: 'rum' },
+        { id: 1, name: 'whiskey', cocktails: ['Manhattan', 'Mint Julep', 'Old Fashioned'] },
+        { id: 2, name: 'gin', cocktails: ['French 75', 'Pink Gin & Tonic', 'Triple Citrus'] },
+        { id: 3, name: 'vodka', cocktails: ['Martini', 'Helsinki Mule', 'Swedish Paloma'] },
+        { id: 4, name: 'tequila', cocktails: ['Honey Bee', 'Sunrise', 'Manhattan'] },
+        { id: 5, name: 'rum', cocktails: ['Punch', 'DarknStormy', 'Mojito'] },
       ]);
     });
 };
+
+
+// 'https://www.liquor.com/recipes/manhattan-2/', 'https://www.liquor.com/recipes/mint-julep/]' 
+// https://www.liquor.com/recipes/bourbon-old-fashioned/
+
+
+// gin 
+// https://www.liquor.com/recipes/amaretto-french-75/
+// https://www.liquor.com/recipes/pink-plymouth-gin-tonic/
+// https://www.liquor.com/recipes/triple-citrus-24/
+
+// vodka 
+// https://www.liquor.com/recipes/absolut-vodka-martini/
+// https://www.liquor.com/recipes/finlandia-vodka-helsinki-mule/
+// https://www.liquor.com/recipes/absolut-swedish-paloma/
+
+// tequila 
+// https://www.liquor.com/recipes/tequila-honey-bee/
+// https://www.liquor.com/recipes/tequila-sunrise/
+// https://www.liquor.com/recipes/cazadores-tequila-manhattan/
+
+// rum
+// https://www.liquor.com/recipes/barbados-rum-punch/
+// https://www.liquor.com/recipes/dark-n-stormy/
+// https://www.liquor.com/recipes/mojito/
